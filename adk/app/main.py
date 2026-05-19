8fdc1375d4aa7035944a1a2b6a4918c1525c4cde
LeonardoCFilho <leonardo.cortes@discente.ufg.br>
2026-05-19 18:31:34 -0300

    update: garantindo o load do .env

--- adk/app/main.py
+++ adk/app/main.py
@@ -6,6 +6,9 @@ from google.adk.cli.fast_api import get_fast_api_app
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+from dotenv import load_dotenv
+load_dotenv()
+
 app: FastAPI = get_fast_api_app(
     agents_dir="design_agents/roles",
     web=True,
